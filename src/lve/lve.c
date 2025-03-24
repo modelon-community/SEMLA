c594e2fed65d4e318c86a01e179a0b6d98d1cacf
Axel Mårtensson <axel.martensson@modelon.com>
2025-03-24 17:18:06 +0000

    feat: add command line options to LVE (LIC-806)

--- src/lve/lve.c
+++ src/lve/lve.c
@@ -32,6 +32,47 @@
 #endif /* __INCLUDE_OPENSSL_APPLINK */
 #endif /* INCLUDE_OPENSSL_APPLINK */
 
+#define STR2(x) #x
+#define STR(x) STR2(x)
+
+static void print_version()
+{
+
+    printf("{\n"
+           "    \"name\": \"%s\",\n"
+           "    \"version\": \"%s\",\n"
+#ifdef TOOLS_LIST_JSON_STRING
+           "    \"tools\": %s,\n"
+#endif
+           "}\n",
+           STR(PROJECT_NAME), STR(PROJECT_VERSION)
+#ifdef TOOLS_LIST_JSON_STRING
+                                  ,
+           TOOLS_LIST_JSON_STRING
+#endif
+    );
+}
+
+static void print_usage(FILE *stream)
+{
+    fprintf(
+        stream,
+        "USAGE: %s [options]\n"
+        "Handles Decryption and Licensing of (Encrypted) Modelica Libraries.\n"
+        "\nOptions:\n"
+        "--checkout-feature <name>   License feature to checkout. Also "
+        "requires setting --libpath.\n"
+        "--libpath <path>   Path to the encrypted Modelica library "
+        "(optional).\n"
+        "                   Path can be either relative from current directory "
+        "or absolute.\n"
+        "--version   Print version information and exit. This must be "
+        "the only option given.\n"
+        "--help   Print usage and exit. This must be the only "
+        "option given.\n",
+        STR(LVETARGET));
+}
+
 #ifdef _MSC_VER
 
 /*
@@ -46,13 +87,48 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 
 #else
 
-int main(void)
+int main(int argc, char **argv)
+{
 
 #endif
 
-{
     struct mlle_lve_ctx lve_ctx = {stdin, stdout, NULL, NULL, 0,
                                    0,     0,      NULL, NULL, NULL};
+
+    char *checkout_feature = NULL;
+    char *libpath = NULL;
+    int i = 0;
+
+    for (i = 1; i < argc; ++i) {
+        if (0 == strcmp(argv[i], "--checkout-feature")) {
+            i++;
+            snprintf(checkout_feature, sizeof(checkout_feature), "%s", argv[i]);
+        } else if (0 == strcmp(argv[i], "--libpath")) {
+            i++;
+            snprintf(libpath, sizeof(libpath), "%s", argv[i]);
+        } else if (0 == strcmp(argv[i], "--version")) {
+            print_version();
+            exit(argc == 2);
+        } else if (0 == strcmp(argv[i], "--help")) {
+            print_usage(stdout);
+            exit(argc == 2);
+        } else {
+            fprintf(stderr,
+                    "Error: Unexpected options. Use '%s --help' for usage.\n",
+                    STR(LVETARGET));
+            exit(1);
+        }
+    }
+    if (checkout_feature != NULL && libpath == NULL) {
+        fprintf(
+            stderr,
+            "Error: --checkout-feature is set, which also requires setting "
+            "--libpath, but --libpath is not set. Please also set --libpath.");
+        return EXIT_FAILURE;
+    } else if (checkout_feature != NULL && libpath != NULL) {
+
+        // mlle_lve_libpath(lve_ctx, command);
+    }
 
     mlle_log_open("SEMLA_LVE_LOG_FILE");
 
